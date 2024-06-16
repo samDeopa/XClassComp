9b75296502e5af76d265b36da0b9e41696066083
Sameer Chand <sam@manishas-MacBook-Air.local>
2024-06-16 16:41:51 +0530

    first commit

---
+++ src/CounterApp.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+class CounterApp extends React.Component {
+  constructor() {
+    super();
+    this.state = { count: 0 };
+  }
+
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrement = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Counter App</h1>
+        <p>Count: {this.state.count}</p>
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
+      </div>
+    );
+  }
+}
+
+export default CounterApp;
